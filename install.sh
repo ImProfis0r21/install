a4c8aee480ca432d50de0368caf660e515fca071
ImProfis0r21 <101109413+ImProfis0r21@users.noreply.github.com>
2022-03-07 14:39:00 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,255 @@
+#!/bin/bash
+clear
+
+ cd .. 
+ rm -rif usr/etc/motd
+ cd $HOME
+ 
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;92m#   this tools creat by ImProfis0r       # \e[0m\n"           
+printf "\e[1;92m#       Join my chanell                # \e[0m\n"
+printf "\e[1;91m#           @PubgxForSalle                     # \e[0m\n"
+printf "\e[1;92m#                                      # \e[0m\n"
+printf "\e[1;92m######################################## \e[0m\n"
+sleep 3
+pkg update -y
+clear
+pkg upgrade -y
+sleep 10
+
+echo "       ImProfis0r  "
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;90m          ImProfis0r ./  \e[0m\n "
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;96m          ImProfis0r ..\  \e[0m\n  "
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m          ImProfis0r .../ \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m           ImProfis0r ....\  \e[0m\n "
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m           ImProfis0r ...../ \e[0m\n "
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m         ImProfis0r ......\ \e[0m\n "
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m          ImProfis0r ......./ \e[0m\n "
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m         ImProfis0r ........\\e[0m\n "
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m        ImProfis0r........./  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m       ImProfis0r  ..........\ "
+printf "\e[1;92m########################################\e[0m\n"
+echo "
+"
+
+termux-setup-storage
+clear
+sleep 2
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m          bust ta pka dawnload abet nano \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+sleep 2
+pkg intsall nano -y
+sleep 2 
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m          nano ba sarkawtuy dawnload bw  \e[0m\n" 
+printf "\e[1;92m########################################\e[0m\n"
+sleep 2
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m          busta ta python dawnload abet \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+sleep 2
+
+pkg install python -y 
+sleep 1
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m          pythonish ba sarkawtuy dawnload bw  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+clear
+
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m           busta ta python2 tawaw abet  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+pkg install python2 -y
+pkg install python3 -y
+sleep 2 
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m                   python3 tawaw bu  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m              bust ba python2 dawnload bet  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m               busta ruby dawnload bet  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+pkg install ruby -y
+sleep 2
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m                  rubish tawaw bw \e[0m\n "
+printf "\e[1;92m########################################\e[0m\n"
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m            busta ba openssh dawnload bet\e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+pkg install openssl-tool  -y
+sleep 2
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m             openssh dawnload bu tawaw  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m              busta ta curl dawnload bet  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+sleep 2
+pkg install curl -y
+sleep 2
+clear 
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m              curlish dawnload bu tawaw  \e[0m\n" 
+printf "\e[1;92m########################################\e[0m\n"
+sleep 2
+clear 
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m              bust ba git dawnload bet  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+pkg install git -y 
+sleep 2 
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m              git ba  tawawy dawnload bw  \e[0m\n  "
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m             busta ta figlet dawnload abet \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+pkg install figlet -y
+sleep 2
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m                figtlet ish tawaw bw  \e[0m\n" 
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m             bust ta unzip tawaw abet  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+pkg install unzip -y
+sleep 2
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m              unzip dawnload bw tawaw  \e[0m\n" 
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m             busta ba zip dawnload bet  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+pkg install zip -y
+clear
+sleep 2
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m                 zip  awnload bw  \e[0m\n" 
+printf "\e[1;92m########################################\e[0m\n"
+clear
+
+cd $HOME 
+cd ..
+rm -rif usr/etc/motd
+cd $HOME
+cd ..
+cd usr/etc
+mkdir shahom
+cd shahom
+echo Slaw Am Rekxstana Lalayan ImProfis0r Krawa >ImProfis0r.txt
+cd $HOME
+cd /sdcard
+mkdir sh4hom
+cd shahowashere 
+echo Slaw Am Rekxstana Lalayan ImProfis0r Krawa >ImProfis0r.txt
+cd $HOME
+pkg install sl -y
+sleep 1
+clear
+printf "\e[1;92m##############################################################\e[0m\n"
+printf "\e[1;91m   Xwat Lagall Tawaw Bw    \e[0m\n"
+printf "\e[1;92m#############################################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m  Xwat Lagall Tawaw Bw  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m  Xwat Lagall Tawaw Bw  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m  Xwat Lagall Tawaw Bw  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m####################################\e[0m\n"
+printf "\e[1;91m   Xwat Lagall Tawaw Bw   \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+
+printf "\e[1;91m  Xwat Lagall Tawaw Bw \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+clear
+sleep 1
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m   Xwat Lagall Tawaw Bw  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+sleep 5
+pkg install tor -y
+cd $HOME
+
+figlet PROFISOR
+xdg-open https://t.me/ImProfis0r
+sleep 10
+
+pkg install php -y
+printf "\e[1;94m########################################\e[0m\n"
+printf "\e[1;94m#   this tools G by ImProfis0r       # \e[0m\n"           
+printf "\e[1;4m#       Xwat Lagall           # \e[0m\n"
+printf "\e[1;94m#           @ImProfis0r                     # \e[0m\n"
+printf "\e[1;94m#                                      # \e[0m\n"
+printf "\e[1;94m######################################## \e[0m\n"
+sleep 3
+figlet good bye 
+printf "\e[1;92m Telegram @ImProfis0r \e[0m\n"\
